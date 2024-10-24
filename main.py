2336df65f67825044bed08702e0ecb0f7b9e4451
Marcin <marcin.kopka1337@gmail.com>
2024-10-24 23:17:20 +0200

    deleting db

--- main.py
+++ main.py
@@ -1,109 +1,52 @@
-<<<<<<< HEAD
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
-
 def main():
-    # Wyświetlanie wycentrowanego loga aplikacji
-    left_co, cent_co, cent_co2, last_co = st.columns(4)
-    with cent_co:
+    # Displaying the centered app logo
+    left_col, center_col, center_col2, right_col = st.columns(4)
+    with center_col:
         st.image('MAGWIZ.png', width=350)
-    # Wyświetlanie tekstu pod logiem z uzyciem kodu HTML
-    st.markdown("<h1 style='text-align: center; color: black;'>Skutecznie zarządzaj zapasami w magazynie</h1>", 
+    
+    # Displaying text below the logo using HTML code
+    st.markdown("<h1 style='text-align: center; color: black;'>Effectively Manage Your Warehouse Inventory</h1>", 
                 unsafe_allow_html=True)
-    # Wyświetlanie kreski oddzielającej
+    
+    # Displaying a dividing line
     st.markdown("---")
-    # Wyświetlanie tekstu pod kreską
+    
+    # Displaying text below the dividing line
     st.write(
         """
-        Prosta w obsłudze aplikacja Magwiz to narzędzie usprawniające przepływ zapasów
-        w magazynie. Wszystkie niezbędne funkcje w jednym miejscu! 
+        The simple-to-use Magwiz app is a tool that streamlines the flow of inventory
+        in the warehouse. All essential functions in one place!
         """)
     
-    # Opis funkcji
-    st.header('Funkcje:')
+    # Function description
+    st.header('Features:')
     st.markdown("""
-    - **Eksploracja Danych (🔍):** Aktualizuj i przeglądaj dane związane z działalnością magazynową, aby lepiej zrozumieć trendy i wzorce.
-    - **Stany Zapasów (📦):** Śledź aktualne stany zapasów produktów oraz monitoruj ich zmiany w czasie.
-    - **Braki Magazynowe (⚠️):** Otrzymuj informację o brakach magazynowych i podejmuj odpowiednie działania w celu ich rozwiązania.
-    - **Analiza ABC (📊):** Analizuj produkty pod kątem ich wartości i znaczenia dla działalności, aby lepiej zarządzać zapasami.
-    - **Terminowość zamówień (🚚):** Monitoruj terminowość dostaw oraz wysyłek oraz identyfikuj opóźnienia w celu optymalizacji procesów logistycznych.
-    - **Czas realizacji zamówień(⏱️):** Monitoruj czas, jaki zajmuje realizacja dostaw oraz zamówień, aby zapewnić terminowe dostawy i optymalizować procesy logistyczne.
-    - **Wypełnienie magazynów (📈):** Sprawdzaj stopień wypełnienia magazynów i podejmuj decyzje dotyczące jego optymalnego wykorzystania.
+    - **Data Exploration (🔍):** Update and browse data related to warehouse activities to better understand trends and patterns.
+    - **Inventory Levels (📦):** Track the current inventory levels of products and monitor their changes over time.
+    - **Stock Shortages (⚠️):** Receive information about stock shortages and take appropriate actions to resolve them.
+    - **ABC Analysis (📊):** Analyze products based on their value and importance to better manage inventory.
+    - **Order Timeliness (🚚):** Monitor the timeliness of deliveries and shipments, and identify delays to optimize logistics processes.
+    - **Order Fulfillment Time (⏱️):** Track the time it takes to fulfill deliveries and orders to ensure timely deliveries and optimize logistics processes.
+    - **Warehouse Fill Levels (📈):** Check warehouse fill levels and make decisions on their optimal usage.
     """)
 
-    st.write("Proszę wybrać jedną z opcji z panelu po lewej stronie.")
+    st.write("Please select one of the options from the panel on the left side.")
 
     show_pages(
         [
-            Page("main.py", "Strona główna", "🏠"),
-            Page("pages/1_eksploracja_danych.py", "Eksploracja danych", "🔍"),
-            Page("pages/2_stany_zapasow.py", "Stany zapasów", "📦"),
-            Page("pages/3_braki_magazynowe.py", "Braki magazynowe", "⚠️"),
-            Page("pages/4_analiza_abc.py", "Analiza ABC", "📊"),
-            Page("pages/5_terminowosc_dostaw.py", "Terminowość zamówień", "🚚"),
-            Page("pages/6_realizacja_dostaw.py", "Czas realizacji zamówień", "⏱️"),
-            Page("pages/7_wypelnienie_magazynow.py", "Wypełnienie magazynów", "📈")
-
+            Page("main.py", "Home", "🏠"),
+            Page("pages/1_data_exploration.py", "Data Exploration", "🔍"),
+            Page("pages/2_inventory_levels.py", "Inventory Levels", "📦"),
+            Page("pages/3_stock_shortages.py", "Stock Shortages", "⚠️"),
+            Page("pages/4_abc_analysis.py", "ABC Analysis", "📊"),
+            Page("pages/5_order_timeliness.py", "Order Timeliness", "🚚"),
+            Page("pages/6_order_fulfillment_time.py", "Order Fulfillment Time", "⏱️"),
+            Page("pages/7_warehouse_fill_levels.py", "Warehouse Fill Levels", "📈")
         ]
     )
 
 if __name__ == "__main__":
     main()
-
-
-=======
-import streamlit as st
-from st_pages import Page, show_pages, add_page_title
-
-
-def main():
-    # Wyświetlanie wycentrowanego loga aplikacji
-    left_co, cent_co, cent_co2, last_co = st.columns(4)
-    with cent_co:
-        st.image('MAGWIZ.png', width=350)
-    # Wyświetlanie tekstu pod logiem z uzyciem kodu HTML
-    st.markdown("<h1 style='text-align: center; color: black;'>Skutecznie zarządzaj zapasami w magazynie</h1>", 
-                unsafe_allow_html=True)
-    # Wyświetlanie kreski oddzielającej
-    st.markdown("---")
-    # Wyświetlanie tekstu pod kreską
-    st.write(
-        """
-        Prosta w obsłudze aplikacja Magwiz to narzędzie usprawniające przepływ zapasów
-        w magazynie. Wszystkie niezbędne funkcje w jednym miejscu! 
-        """)
-    
-    # Opis funkcji
-    st.header('Funkcje:')
-    st.markdown("""
-    - **Eksploracja Danych (🔍):** Aktualizuj i przeglądaj dane związane z działalnością magazynową, aby lepiej zrozumieć trendy i wzorce.
-    - **Stany Zapasów (📦):** Śledź aktualne stany zapasów produktów oraz monitoruj ich zmiany w czasie.
-    - **Braki Magazynowe (⚠️):** Otrzymuj informację o brakach magazynowych i podejmuj odpowiednie działania w celu ich rozwiązania.
-    - **Analiza ABC (📊):** Analizuj produkty pod kątem ich wartości i znaczenia dla działalności, aby lepiej zarządzać zapasami.
-    - **Terminowość zamówień (🚚):** Monitoruj terminowość dostaw oraz wysyłek oraz identyfikuj opóźnienia w celu optymalizacji procesów logistycznych.
-    - **Czas realizacji zamówień(⏱️):** Monitoruj czas, jaki zajmuje realizacja dostaw oraz zamówień, aby zapewnić terminowe dostawy i optymalizować procesy logistyczne.
-    - **Wypełnienie magazynów (📈):** Sprawdzaj stopień wypełnienia magazynów i podejmuj decyzje dotyczące jego optymalnego wykorzystania.
-    """)
-
-    st.write("Proszę wybrać jedną z opcji z panelu po lewej stronie.")
-
-    show_pages(
-        [
-            Page("main.py", "Strona główna", "🏠"),
-            Page("pages/1_eksploracja_danych.py", "Eksploracja danych", "🔍"),
-            Page("pages/2_stany_zapasow.py", "Stany zapasów", "📦"),
-            Page("pages/3_braki_magazynowe.py", "Braki magazynowe", "⚠️"),
-            Page("pages/4_analiza_abc.py", "Analiza ABC", "📊"),
-            Page("pages/5_terminowosc_dostaw.py", "Terminowość zamówień", "🚚"),
-            Page("pages/6_realizacja_dostaw.py", "Czas realizacji zamówień", "⏱️"),
-            Page("pages/7_wypelnienie_magazynow.py", "Wypełnienie magazynów", "📈")
-
-        ]
-    )
-
-if __name__ == "__main__":
-    main()
-
-
->>>>>>> origin/main
